e92bab4f13211a56adfb82f4ff19496d00221859
JesseShawCodes <JesseShawCodes@gmail.com>
2023-08-28 16:57:06 -0400

    Add name to user Sign Up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
         puts "TESTING~!!!!"
         devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
 end

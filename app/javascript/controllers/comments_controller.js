240e2b9fd5b95d9ce3a0bfd8f5a1747a509ca784
JesseShawCodes <JesseShawCodes@gmail.com>
2023-08-31 17:03:27 -0400

    Button behavior changes

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -18,5 +18,22 @@ export default class extends Controller {
 
     const commentBody = document.getElementById(bodyId)
     commentBody.classList.toggle("d-none")
+
+    this.toggleEditButon(document.getElementById(event.params["edit"]))
+  }
+
+  toggleEditButon(editButton) {
+    if(editButton.innerText === "Edit") {
+      editButton.innerText = "Cancel"
+      editButton.classList.toggle("bg-secondary")
+    }
+    else {
+      editButton.innerText = "Edit"
+      editButton.classList.toggle("bg-warning")
+    }
+  }
+
+  toggleEditButtonText(editButton) {
+    console.log("toggleEditButtonText")
   }
 }

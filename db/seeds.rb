d44a8aef64e75ea3a368b647b949b141074180c4
JesseShawCodes <JesseShawCodes@gmail.com>
2023-09-06 13:07:11 -0400

    New Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'person_dataset.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  p = Person.new
+  p.name = row['name']
+  p.title = row['title']
+  p.content = row['content']
+  p.save
+
+  puts "#{p.name} saved!"
+end
